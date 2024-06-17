39e62d3e8bc8fb267f6a455a2f62bef7cdbbbe24
manthanbariya <115332724+manthanbariya@users.noreply.github.com>
2024-06-18 00:24:33 +0530

    Add files via upload

---
+++ Start Project Parallax Scrolling Website/script7.js
@@ -0,0 +1,45 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+window.addEventListener('scroll',()=>{
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 1+'px';
+    leaf.style.top = value * -1.5+'px';
+    hill5.style.left = value * 1.5+'px';
+    hill4.style.left = value * -1.5+'px';
+    hill1.style.top = value * -1.5+'px';
+
+});
+const apiKey = '765a5d95b95ffdaa1677cfbfd9518b11';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
